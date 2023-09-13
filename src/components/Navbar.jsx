059bd9f7ebdc4197c9cde40387eb518677436f58
Kumar Hrithik <hrithikk2765@gmail.com>
2023-09-14 00:57:41 +0530

    solved issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="my-4 rounded flex bg-white h-[60px] gap-2 justify-center items-center font-medium">
-      <img src="src/assets/firebase.svg" />
+      <img src="assets/firebase.svg" />
+
       <h1 className="text-xl ">Firebase Contacts App</h1>
     </div>
   );
